6d304fdd43576e0f5a07cdd66be912a00a747c9a
saicharanbhogi2001 <84128500+saicharanbhogi2001@users.noreply.github.com>
2021-06-10 15:08:48 +0530

    Add files via upload

---
+++ answer 1.py
@@ -0,0 +1,21 @@
+def main(str1, str2):
+   m=len(str1)
+   n=len(str2)
+   j= 0 
+   i=0
+   while j < m and i < n:
+       if str1[j] == str2[i]:
+           j+=1
+       i+=1
+   return j==m
+
+
+
+str2=str(input())
+N=int(input())
+for i in range(N):
+   str1=str(input())
+   if main(str1, str2):
+       print("POSITIVE")
+   else:
+       print( "NEGATIVE")
